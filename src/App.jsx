6180ab930af3b152556b596d3834e26ea7d777fd
JordanGidman <jordanleegidman@hotmail.com>
2023-09-15 14:40:11 +0100

    Navigation and redirection
    There was an issue where once you had clicked start tracking if you did not first select the cities tab before trying to do anything else it would cause bugs in the application. Now as soon as the index route is hit the navigate component will redirect the URL to app/cities preventing the user from not being on the right link.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -43,10 +43,9 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          {/* The Navigate comp here is essentially a redirect so as soon as this index element is hit we will be redirected to the url app/cities 
+          the replace keyword ensures the back button works correctly */}
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
