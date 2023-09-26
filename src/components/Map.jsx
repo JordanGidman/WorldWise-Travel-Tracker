1cb2e4f123a4be28a988bac28412970f8a4454aa
JordanGidman <jordanleegidman@hotmail.com>
2023-09-26 16:02:37 +0100

    Map Ground Work
    I have added a functional map for this application using the leaflet and react-leaflet libraries.
    
    I have begun laying the ground work for the maps interactivity. Right now selecting a city from the sidebar will move the map to that city as well as clicking somewhere which at the moment will bring up the form for adding a new location but as yet cannot add a new city.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,31 +1,81 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useEffect, useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   //a hook we get from react router that allows us to navigate to a specific place without a link such as the below
-  const navigate = useNavigate();
 
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  console.log(useSearchParams);
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
+
+  console.log(setSearchParams);
+
+  //This effect will sync the map postion with the currently selected city so that the map will update accordingly
+  useEffect(
+    function () {
+      console.log(mapLat, mapLng);
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        //Here we use the navigate function returned from the hook to change the url to /form on the end which renders our form this is imperative where as using the navLink is a declarative way
-        navigate("form");
-      }}
-    >
-      <h1>Map</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
-        Change Pos
-      </button>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
